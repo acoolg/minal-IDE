33ccb259fbb084a60b9f9853b28640cbcb39beff
acoolg <vecy11332@gmail.com>
2024-12-08 21:53:24 +0800

    tauri fix

--- src/jump.js
+++ src/jump.js
@@ -2,9 +2,9 @@ import { getCurrentWindow } from "@tauri-apps/api/window";
 
 const appwindow = getCurrentWindow();
 window.onload = async () => {
-    await appwindow.setResizable(false)
+    await appwindow.setResizable(false);
     setTimeout(async () => {
-        await appwindow.setResizable(true)
-        window.location.href = "src/sheet/main.html";
+        await appwindow.setResizable(true);
+        window.location.href = "./sheet/main.html";
     }, 2000);
 };
